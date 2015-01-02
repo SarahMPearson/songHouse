c07f77b3d20b33a6e76087b354eff8a2bfcf61de
Sarah Pearson <sarahp@me.com>
2015-01-01 21:07:55 -0600

    updated add publisher and writer form

--- db/functions/query_by_writersname.sql
+++ db/functions/query_by_writersname.sql
@@ -9,4 +9,4 @@ songs
 INNER JOIN percentages ON percentages.song_id = songs.id
 INNER JOIN writers ON writers.id = percentages.writer_id
 INNER JOIN publishers on publishers.id = percentages.publisher_id
-WHERE percentages.writer_id = 2; -- request.payload.writer_id?
+WHERE percentages.writer_id = 24;
--- db/functions/query_songs.sql
+++ db/functions/query_songs.sql
@@ -1,18 +1,18 @@
-create or replace function query_songs (uid integer)
-returns table (title varchar, "songID" integer[], doc date[], percent integer[], "writerName" varchar[], "pubName" varchar[], pro varchar[]) AS $$
-declare
-begin
+  create or replace function query_songs (uid integer)
+  returns table (title varchar, "songID" integer[], doc date[], percent integer[], "writerName" varchar[], "pubName" varchar[], pro varchar[], "writerID" integer[] ) AS $$
+  declare
+  begin
 
-RETURN query
-  SELECT songs.title, array_agg(songs.id) as "songID", array_agg(distinct songs.doc) as "doc", array_agg(percentages.percent) as "percents", array_agg(writers.name) as "writerName", array_agg(publishers.name) as "pubName", array_agg(writers.pro) as "PRO"
-  FROM songs
-  INNER JOIN percentages ON percentages.song_id = songs.id
-  INNER JOIN writers ON writers.id = percentages.writer_id
-  INNER JOIN publishers ON publishers.id = percentages.publisher_id
-  WHERE percentages.user_id = uid
-  GROUP BY songs.title
-  ORDER By songs.title;
+  RETURN query
+    SELECT songs.title, songs.id as "songID", array_agg(distinct songs.doc) as "doc", array_agg(percentages.percent) as "percents", array_agg(writers.name) as "writerName", array_agg(publishers.name) as "pubName", array_agg(writers.pro) as "PRO", array_agg(writers.id) as "writerID"
+    FROM songs
+    INNER JOIN percentages ON percentages.song_id = songs.id
+    INNER JOIN writers ON writers.id = percentages.writer_id
+    INNER JOIN publishers ON publishers.id = percentages.publisher_id
+    WHERE percentages.user_id = uid
+    GROUP BY songs.title, songs.id
+    ORDER By songs.title;
 
 
-end;
-$$ language plpgsql;
+  end;
+  $$ language plpgsql;
